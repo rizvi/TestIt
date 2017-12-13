da93912bb813cc61c97073c1621b55751a927043
zakir_rizvi <rizvi3389ict@gmail.com>
2017-12-13 21:58:02 +0600

    Initial Project Setup

---
+++ src/com/rizvi/dd/HelloKhan.java
@@ -0,0 +1,14 @@
+package com.rizvi.dd;
+
+public class HelloKhan {
+
+	public HelloKhan() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Rizvi!!!");
+	}
+
+}
